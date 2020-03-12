10ac7b6498f1b726bc28aa82b33503ee37adb4a8
stasonli <stasonli29@gmail.com>
2020-03-12 15:00:43 +0900

    created defaultStyle and defaultTemplate

--- view/homeView.php
+++ view/homeView.php
@@ -1,39 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <title>noraecheck</title>
-	    <link rel="stylesheet" href="../public/css/style.css"/> 
-    </head> 
-    <body>
-        <div id="mainWrapper">
-            <header id="banner">
-                <div id="logo">Logo</div>
-                Banner Picture
-            </header>
-            <section id="mainContent">
-                My List
-            </section>
-            <nav id="nav">
-                <ul>
-                    <li>
-                        <img src="../public/images/search.png"/>
-                        <div>Search</div>
-                    </li>
-                    <li>
-                        <img src="../public/images/profile.png"/>
-                        <div>Profile</div>
-                    </li>
-                    <li>
-                        <img src="../public/images/challenge.png"/>
-                        <div>Challenge</div>
-                    </li>
-                    <li>
-                        <img src="../public/images/songList.png"/>
-                        <div>My List</div>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </body>
-</html>
+<?php $title = 'noraecheck';?>
+<?php $style = 'defaultStyle.css';?>
+
+<?php ob_start();?>
+<div class="mainWrapper homePage">
+    <!-- include banner php file instead of header tag -->
+    <!-- <php inlcude(); ?> -->
+    <header id="banner">
+        <div id="logo">Logo</div>
+        Banner Picture
+    </header>
+    <section id="mainContent">
+        <!-- add divs with the list of songs from js function -->
+    </section>
+    <nav id="nav">
+        <ul>
+            <li class="neonBlue">
+                <img src="../public/images/search.png"/>
+                <div>Search</div>
+            </li>
+            <li class="neonBlue">
+                <img src="../public/images/profile.png"/>
+                <div>Profile</div>
+            </li>
+            <li class="neonBlue">
+                <img src="../public/images/challenge.png"/>
+                <div>Challenge</div>
+            </li>
+            <li class="neonBlue">
+                <img src="../public/images/songList.png"/>
+                <div>My List</div>
+            </li>
+        </ul>
+    </nav>
+</div>
+<?php $content = ob_get_clean();?>
+<?php require('defaultTemplate.php');?>
