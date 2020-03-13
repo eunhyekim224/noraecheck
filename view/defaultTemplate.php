2f6ea9f1e17af5bbb6852451a8f27a362307e934
thatguy1155 <jamesglass@Jamess-MacBook-Pro.local>
2020-03-13 16:07:17 +0900

    fixed the stylesheet url to what it should be

--- view/defaultTemplate.php
+++ view/defaultTemplate.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <title><?= $title?></title>
-        <link rel="stylesheet" href="../public/css/<?= $style?>"/>
+        <link rel="stylesheet" href="public/css/<?= $style?>"/>
         <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Baloo+2&display=swap"/>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Gamja+Flower&display=swap"/> -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Jua&display=swap"/> 
