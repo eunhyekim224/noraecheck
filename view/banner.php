c7ac16b0987a13463176b2bcdec75783945a8a94
JeremyBeyond <JeremyBeyond@Helens-MacBook-Pro.local>
2020-03-13 14:31:14 +0900

    banner add

---
+++ view/banner.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="../public/css/banner.css" rel="stylesheet">
+<title>노래책</title>
+
+</head>
+
+<body>
+
+<div class='banner'>
+<img src="../public/images/logo.png" alt='logo' id='logo'/>
+
+<p>
+<span data-text="noraecheck" class="word">noraecheck</span>
+</p>
+
+<p>
+<span data-text="" class="word"></span>
+</p>
+
+</div>
+</body>
+</html>
+
+
