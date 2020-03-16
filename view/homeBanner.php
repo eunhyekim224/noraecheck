1cd3748c0372d1e615ae44d3b32d36230bccbf03
JeremyBeyond <JeremyBeyond@Helens-MacBook-Pro.local>
2020-03-16 11:35:38 +0900

    test

--- view/homeBanner.php
+++ view/homeBanner.php
@@ -1,3 +1,4 @@
+
 <div id='banner'>
     <p id="bannerImage"><img src="./public/images/logo.png" alt='logo'/></p>
     <p id="bannerText"><span data-text="noraecheck" class="word">noraecheck</span></p>
