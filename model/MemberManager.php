9fc85763d584bb065adbfe19e45560be4e7e1456
JeremyBeyond <JeremyBeyond@Helens-MacBook-Pro.local>
2020-03-19 14:30:44 +0900

    test

--- model/MemberManager.php
+++ model/MemberManager.php
@@ -14,23 +14,23 @@
             $status = $addMember->execute(array(
                 'email' => htmlspecialchars($email),
                 'username' => htmlspecialchars($username),
-                'password' =>  password_hash(htmlspecialchars($password, PASSWORD_DEFAULT))
+                'password' =>  password_hash(htmlspecialchars($password), PASSWORD_DEFAULT)
             ));
             if (!$status) {
                 throw new PDOException('Impossible to add the member!');
             }
         } 
-        public function getMember($username, $password) {
+        public function getMember($username) {
             $db = $this->dbConnect();
             $members = $db->prepare("SELECT id, username, password FROM members WHERE username = :username");
             $resp = $members->execute(array(
-                'username' => $username,
-                'password' => $password
+                'username' => $username
             ));
             if(!$resp) {
                 throw new PDOException('Invalid username or password!');
             }
-            return $members;
+            $memFetch = $members->fetch();
+            return $memFetch;
         }
 
 
