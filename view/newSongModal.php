e21c67a68b341a54d39ff3766a02f1c04e433281
thatguy1155 <jamesglass@Jamess-MacBook-Pro.local>
2020-03-24 22:47:32 +0900

    updated search modal 3/24

--- view/newSongModal.php
+++ view/newSongModal.php
@@ -1,43 +1,25 @@
 <div class="modalSearch gothamPro">
     <div class="modalContentSearch">
-        <!-- <p>add song to playlist</p> -->
-        
-        <form method="post" action="index.php" id="searchNewPlaylist">
-            <input type="text" name="playlistName" class = "playlistName"id="playlistName" size="30" maxlength="50" required/>
-            <input type="hidden" name="action" value="addSongToNewPlaylist">
-            <input type="hidden" name="song" value=<?=urlencode($song); ?>>
-            <input type="hidden" name="singer" value=<?=urlencode($singer); ?>>
-            <input type="hidden" name="tj" value=<?=$tj; ?>>
-            <input type="hidden" name="kumyoung" value=<?=$kumyoung; ?>>
-            <img src="public/images/plusWhite.png" id="searchNewPlaylistSubmit"/>
-        </form>
-        
         <form method="post" action="index.php">
-            
-        
-            <input type="hidden" name="action" value="addToPlaylist">
+            <div id= 'searchNewPlaylist'>
+                <input type="text" name="playlistName" class = "playlistName"id="playlistName" size="30" maxlength="50" required/>
+            </div>
+            <input type="hidden" name="action"id="modalFormAction" value="addToPlaylist">
             <label for="playlistId"><span>add song to playlist</span></label>
             <div id="playlistId">
-            
-                
                 <?php while ($playlist = $playlistsAdd->fetch()) { ?>
-                    <?= '<option value="' .$playlist['playlistId'] .'">' .$playlist['playlistName'] .'</option>';?>
-                    
-                
+                    <?= '<input type="radio" name="playlistId" id="' .$playlist['playlistId']  .'" value="' .$playlist['playlistId'] .'"/><label class="playlists" for="' .$playlist['playlistId']  .'">'.$playlist['playlistName'] .'</label>';?>
                 <?php } ?>
-                
-
             </div>
-            
             <div class="modalButtons openSans addSongToNewPlaylistButton">
-                <div name="newPlaylist" class="addPlaylistSearchBtn"><img src="./public/images/plusWhite.png"/></div>             
+                <div name="newPlaylist" class="addPlaylistSearchBtn">new playlist</div>             
             </div>
             <input type="hidden" name="song" value=<?=urlencode($song); ?>>
             <input type="hidden" name="singer" value=<?=urlencode($singer); ?>>
             <input type="hidden" name="tj" value=<?=$tj; ?>>
             <input type="hidden" name="kumyoung" value=<?=$kumyoung; ?>>
             <div class="modalButtons openSans">
-                <input type="submit" name="add to playlist" value="add to playlist" class="btn btnBlue">
+                <input type="submit" name="addToPlaylist" value="add to playlist" class="btn btnBlue">
                 <input type="button" name="cancel" value="Cancel" class="btn btnBlue">               
             </div>
             
