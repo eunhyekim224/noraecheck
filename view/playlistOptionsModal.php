6674fe4c5e933b5591adacc4ff3df8a6814bc378
stasonli <stasonli29@gmail.com>
2020-03-25 16:25:38 +0900

    playlistOptions modal update

--- view/playlistOptionsModal.php
+++ view/playlistOptionsModal.php
@@ -2,25 +2,25 @@
     <div class="modalContent" id="modalContent">
         <div class="modalButtons openSans" id="mainOptions">
             <div>
-                <?= "<a href=\"index.php?action=search&playlistId=" .$_SESSION['playlistId']."\">"; ?>
+                <?= "<a href=\"index.php?action=search&playlistId=" .$playlist['playlistId']."\">"; ?>
                     <input type="button" name="addSong" value="Add songs" class="btn btnBlue"> 
                 </a>        
             </div>
             <form method="post" action="index.php" id="editPlaylistForm">
                 <input type="hidden" name="action" value="editPlaylist"/>
-                <input type="hidden" name="playlistId" value="<?=$_SESSION['playlistId']; ?>" />
+                <input type="hidden" name="playlistId" value="<?=$playlist['playlistId']; ?>" />
                 <div class="albumIcon">
                     <img src="public/images/album2.png" id="mainPlaylistImg" title="Album icon">
                     <div>Edit</div>
                 </div>
                 <div id="editPlaylistInfo">
-                    <input type="text" name="newPlaylistName" id="newPlaylistName" value="<?= $_GET['playlistName']; ?>" />
+                    <input type="text" name="newPlaylistName" id="newPlaylistName" value="<?= $playlist['playlistName']; ?>" />
                     <p>by <?=  $_GET['username']; ?></p>
                     <p>
                         <i class="fas fa-music darkGrey" title="number of songs"></i>
-                        <span class="darkGrey"><?= $_GET['songCount'];?></span>
+                        <span class="darkGrey"><?= $playlist['songCount'];?></span>
                         <i class="far fa-calendar-alt darkGrey" title="creation date"></i>
-                        <span class="darkGrey"><?= $_GET['playlistCreationDate']; ?></span>
+                        <span class="darkGrey"><?= $playlist['playlistCreationDate']; ?></span>
                     </p>
                 </div>
                 <div id="editBtns">
