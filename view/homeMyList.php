ee66e826f40f0911efd8cf097f744914afac1f1b
JeremyBeyond <JeremyBeyond@Helens-MacBook-Pro.local>
2020-03-30 22:03:00 +0900

    homeMyList fix

--- view/homeMyList.php
+++ view/homeMyList.php
@@ -1,3 +1,4 @@
+
 <h1>my playlists</h1> 
 <!-- $_SESSION['username']-->
 <ul class="myList">
@@ -11,11 +12,22 @@
 <?php while ($playlist = $playlists->fetch()): ?>
     <li>
         <div class="myListIcons">
-            <input type="image" name="newPlaylistBtn" id="newPlaylistBtn" src="public/images/plusIcon2.png">
+            <img src="public/images/album2.png" id="playListImg">
         </div>
-        <div id="createTxt">Create a new playlist</div>
-        <?= include('newPlaylistModal.php'); ?>
+        <?= '<a href="index.php?action=showMySongs&playlistId=' .$playlist['playlistId'].'" class= "displayedPlaylists">';?>
+            <div class="playlistInfo">
+                <p class="darkGrey" class="playlistNameText"><?= $playlist['playlistName']; ?></p>
+                <p>by <?= $playlist['username']; ?></p>
+                <p>
+                    <i class="fas fa-music darkGrey" title="number of songs"></i>
+                    <span class="darkGrey"><?= $playlist['songCount'];?></span>
+                    <i class="far fa-calendar-alt darkGrey" title="creation date"></i>
+                    <span class="darkGrey"><?= $playlist['playlistCreationDate']; ?></span>
+                </p>
+            </div>
+        <?= "</a>";?>
     </li>
 <?php endwhile; ?>
 </ul>
 <script src="./public/js/modalNewPlaylist.js"></script>
+
