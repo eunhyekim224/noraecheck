5ec1fb537b241ad1dce85e47ec90b01838dceefa
JeremyBeyond <JeremyBeyond@Helens-MacBook-Pro.local>
2020-04-03 09:25:40 +0900

    modal tweak

--- view/newChallenge.php
+++ view/newChallenge.php
@@ -11,22 +11,4 @@
         </div>
     </div>
 </div>
-
-<!-- <div id="modalAgain" class="modal">
-<div class="modal" id="playlistOptionsModal">
-    <div class="modalContent" id="modalContent">
-        <div class="modalButtons openSans" id="mainOptions">
-            <div>
-                <a href="index.php?action=newChallenge" >
-                    <input type="button" name="newChallenge" value="New Challenge?" class="btn btnBlue"> 
-                </a>    
-                <input type="reset" name="reset" id="reset" value="Repeat Challenge" class="btn btnBlue">       
-            </div>
-            <div>
-                <input type="button" id="cancelBtn" value="cancel" class="btnEnd btnOrange">           
-        </div>
-    </div>
-</div>
-</div> -->
-
 <script src="./public/js/endChallenge.js"></script>
