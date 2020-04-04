95629966225c497123663afb0d5e86c2f1e23410
stasonli <stasonli29@gmail.com>
2020-04-04 15:48:37 +0900

    fixed bug with editProfile modal

--- view/editProfileModal.php
+++ view/editProfileModal.php
@@ -1,4 +1,4 @@
-<div class="modal" style="display:<?php if(isset($errors) AND isset($errors['contextUp']) AND $errors['contextUp'] == "editProfile")  { echo "block";} ?>">
+<div class="modal" style="display:<?php if(isset($errors) AND isset($errors['context']) AND $errors['context'] == "editProfile")  { echo "block";} ?>">
     <div class="modalContent editProfileVerifyModal">
         <p class="profileformP">
             <label id="pwdProfile" for="oldPwd">Password to verify</label>
